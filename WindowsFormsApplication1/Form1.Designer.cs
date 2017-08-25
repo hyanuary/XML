8b52011cc293c757b53a1a2e1f3c7e132ff0eeda
SAE\hyanuary <1003495@student.sae.edu.au>
2017-08-25 12:36:49 +1000

    pushing xml data

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxData1 = new System.Windows.Forms.TextBox();
+            this.textBoxData2 = new System.Windows.Forms.TextBox();
+            this.textBoxData3 = new System.Windows.Forms.TextBox();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxData1
+            // 
+            this.textBoxData1.Location = new System.Drawing.Point(98, 157);
+            this.textBoxData1.Name = "textBoxData1";
+            this.textBoxData1.Size = new System.Drawing.Size(211, 26);
+            this.textBoxData1.TabIndex = 0;
+            this.textBoxData1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxData2
+            // 
+            this.textBoxData2.Location = new System.Drawing.Point(98, 232);
+            this.textBoxData2.Name = "textBoxData2";
+            this.textBoxData2.Size = new System.Drawing.Size(211, 26);
+            this.textBoxData2.TabIndex = 1;
+            // 
+            // textBoxData3
+            // 
+            this.textBoxData3.Location = new System.Drawing.Point(98, 304);
+            this.textBoxData3.Name = "textBoxData3";
+            this.textBoxData3.Size = new System.Drawing.Size(211, 26);
+            this.textBoxData3.TabIndex = 2;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(173, 369);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCreate.TabIndex = 3;
+            this.buttonCreate.Text = "Save";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1150, 775);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.textBoxData3);
+            this.Controls.Add(this.textBoxData2);
+            this.Controls.Add(this.textBoxData1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxData1;
+        private System.Windows.Forms.TextBox textBoxData2;
+        private System.Windows.Forms.TextBox textBoxData3;
+        private System.Windows.Forms.Button buttonCreate;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!File.Exists("data.xml"))
+                {
+                    Information info = new Information();
+                    info.Data1 = textBoxData1.Text;
+                    info.Data2 = textBoxData2.Text;
+                    info.Data3 = textBoxData3.Text;
+                    SaveXML.SaveData(info, "data.xml");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(File.Exists("data.xml"))
+            {
+                XmlSerializer xs = new XmlSerializer(typeof(Information));
+                FileStream read = new FileStream("data.xml", FileMode.Open, FileAccess.Read, FileShare.Read);
+                Information info = (Information)xs.Deserialize(read);
+                textBoxData1.Text = info.Data1;
+                textBoxData2.Text = info.Data2;
+                textBoxData3.Text = info.Data3;
+            }
+
+            
+        }
+    }
+}
